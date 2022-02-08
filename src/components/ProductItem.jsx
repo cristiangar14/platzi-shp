0fe23a74ac6dd53ab9733b40f675c45a40cf6172
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2022-02-08 10:19:21 -0500

    first commit

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import '@styles/ProductItem.scss';
+import AppContext from '@context/AppContext'
+import iconAddToCart from '@icons/bt_add_to_cart.svg';
+
+const ProductItem = ({product}) => {
+
+	const {addToCard} = useContext(AppContext);
+
+	const handleClik = item => {
+		addToCard(item);
+	};
+	return (
+		<div className="ProductItem">
+			<img src={product.images[0]} alt={product.title} />
+			<div className="product-info">
+				<div>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
+				</div>
+				<figure onClick={() => handleClik(product)}>
+					<img src={iconAddToCart} alt="" />
+				</figure>
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
